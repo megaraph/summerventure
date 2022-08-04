3be8653ec1008beb514644796443e2213ec51193
megaraph <47447126+megaraph@users.noreply.github.com>
2022-08-05 01:18:49 +0800

    add authentication views

--- users/views.py
+++ users/views.py
@@ -6,14 +6,32 @@ from .forms import RegistrationForm
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("challenges:explore")
+
     context = {"message": "sup"}
     return render(request, "users/login.html", context=context)
 
 
 def signup_view(request):
-    form = RegistrationForm()
+    if request.user.is_authenticated:
+        return redirect("challenges:explore")
+
+    form = RegistrationForm(data=request.POST or None)
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        return redirect("signup")
 
     context = {
         "form": form,
     }
     return render(request, "users/signup.html", context=context)
+
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+    return redirect("login")
