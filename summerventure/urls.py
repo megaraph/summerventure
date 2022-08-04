85338c1a4d002ac88f9f3c9f15392ff75b50eea4
megaraph <47447126+megaraph@users.noreply.github.com>
2022-08-05 00:03:10 +0800

    add new url routes for auth and challenges

--- summerventure/urls.py
+++ summerventure/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from users.views import login_view, signup_view
 
 urlpatterns = [
+    path("challenges/", include("challenges.urls")),
+    path("login/", login_view, name="login"),
+    path("sign-up/", signup_view, name="signup"),
     path("admin/", admin.site.urls),
 ]
