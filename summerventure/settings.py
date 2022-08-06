195b7ce7d37552e7ea0eb13234a13d1e8f684a7b
megaraph <47447126+megaraph@users.noreply.github.com>
2022-08-06 09:13:08 +0800

    ready for prod

--- summerventure/settings.py
+++ summerventure/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 
 from pathlib import Path
-from telnetlib import DEBUGLEVEL
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,3 +168,5 @@ LOGIN_URL = "/login/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
